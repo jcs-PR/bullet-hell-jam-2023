de4b14f1a007fbe480ac87bf18ef1cb3b3fabf0f
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-04-30 15:22:38 +0100

    changes to objects.

--- dev rats game/Assets/Scripts/Player/Player1Shooting.cs
+++ dev rats game/Assets/Scripts/Player/Player1Shooting.cs
@@ -7,18 +7,30 @@ using UnityEngine;
 public class Player1Shooting : MonoBehaviour
 {
     private BulletManager _bm;
+
+    private int _bulletAmount;
+    [SerializeField] private int maxBulletAmount = 100; 
+    
     // Start is called before the first frame update
     void Start()
     {
         _bm = GetComponent<BulletManager>();
+
+        _bulletAmount = maxBulletAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ShootNewBullet();
+    }
+
+    private void ShootNewBullet()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             _bm.Spawn(transform.position, _bm.Plane == BulletPlane.XY ? transform.up : transform.forward);
+            _bulletAmount -= 1;
         }
     }
 }
