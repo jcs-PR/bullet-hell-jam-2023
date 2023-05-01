74a9b062370c2f16b031eed411ebae502908f564
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-05-01 22:15:04 +0100

    Fixed enemy and bullets.

--- dev rats game/Assets/Scripts/Enemy/LookTowardsPlayer.cs
+++ dev rats game/Assets/Scripts/Enemy/LookTowardsPlayer.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
@@ -11,15 +13,25 @@ public class LookTowardsPlayer : MonoBehaviour
     
     [SerializeField] private Transform player;
 
-    private Vector3 _offset = new Vector3(0, 180, 0);
-    
+    private float _offset = 0;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (player != null)
         {
-            transform.up = Vector3.RotateTowards(transform.up, player.position - transform.position,
-                maximumTurnSpeed, 0);
+            var playerPos = player.position - transform.position;
+            playerPos = Quaternion.Euler(0, 0, _offset) * playerPos;
+            transform.up = Vector3.RotateTowards(transform.up, playerPos,
+                maximumTurnSpeed * Time.deltaTime, 0);
+
         }
 
     }
