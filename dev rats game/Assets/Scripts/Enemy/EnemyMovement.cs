9d78c99fe02090ccefc780f0257ea7c2e2477ee4
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-05-04 13:22:25 +0100

    Added lvl00.

--- dev rats game/Assets/Scripts/Enemy/EnemyMovement.cs
+++ dev rats game/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,24 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2D;
+    [FormerlySerializedAs("_rigidbody2D")] [SerializeField] private Rigidbody2D rigidbody2D;
 
-    [SerializeField] private float waitTimer = 3; 
+    private float _enemyTimer = 0;
+    [SerializeField] private float startingEnemyTimer = 10;
+    
+    [SerializeField] private float minimumMovement = 1;
+    [SerializeField] private float maximumMovement = 3;
+
+    [SerializeField] private float movementSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody2D.GetComponent<Rigidbody2D>();
-        StartCoroutine(MoveEnemy());
+        _enemyTimer = startingEnemyTimer;
     }
 
-    public IEnumerator MoveEnemy()
+    private void Update()
     {
-       yield return new WaitForSeconds(waitTimer);
-       Vector2 currentPosition = _rigidbody2D.position;
-       Vector2 newPosition = new Vector2(Random.Range(0, 5), Random.Range(0, 5)); // Placeholder, to be revised. 
-       _rigidbody2D.MovePosition(currentPosition + newPosition);
+        if (_enemyTimer > 0)
+        {
+            _enemyTimer -= Time.deltaTime;
+        }
+        else
+        {
+            MoveEnemy();
+        }
     }
+
+    private void MoveEnemy()
+    {
+        Vector2 currentPosition = rigidbody2D.position;
+       Vector2 newPosition = new Vector2(Random.Range(minimumMovement, maximumMovement), Random.Range(minimumMovement, maximumMovement)); // Placeholder, to be revised. 
+       rigidbody2D.MovePosition(currentPosition + newPosition * movementSpeed * Time.deltaTime);
+       _enemyTimer = startingEnemyTimer;
+    }
+    
+    
+    
 }
--- dev rats game/Assets/Scripts/HUD/HUDScript.cs
+++ dev rats game/Assets/Scripts/HUD/HUDScript.cs
@@ -22,11 +22,6 @@ public class HUDScript : MonoBehaviour
 
     [SerializeField] private GameObject pausePanel;
 
-    private void Awake()
-    {
-        
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -92,6 +87,7 @@ public class HUDScript : MonoBehaviour
 
     public void ResetGame()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(firstLevelName);
     }
 
