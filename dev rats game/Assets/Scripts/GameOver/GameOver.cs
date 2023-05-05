de3a050ab5f90d66b37f2dd4fc71ff74a22bb9a0
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-05-05 18:14:00 +0100

    Attempts to fix the hud on lvl00.

---
+++ dev rats game/Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] private string lvl00Name = "lvl00";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(lvl00Name);
+    }
+}
