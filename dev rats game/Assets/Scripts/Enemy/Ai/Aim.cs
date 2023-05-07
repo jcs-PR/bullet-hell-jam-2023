8bbbc739b8fb8419f0e44293ed7d47d1f10bfe26
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-05-07 09:24:56 +0100

    Fix failed, to revert.

--- dev rats game/Assets/Scripts/Enemy/Ai/Aim.cs
+++ dev rats game/Assets/Scripts/Enemy/Ai/Aim.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,29 +6,26 @@ using UnityEngine.Serialization;
 
 public class Aim : MonoBehaviour
 {
-    [SerializeField] public Transform player;
+    [SerializeField] private Shoot shoot;
+    [SerializeField] private Transform player;
     [SerializeField] private float playerRadius;
     [Tooltip("frames skiped while predicting position")]
-    [Range(1, 20)]
+    [Range(1f, 20f)]
     [FormerlySerializedAs("_frames")] [SerializeField] private int frames = 10;
-    [Range(1, 100)]
-    [FormerlySerializedAs("_steps")][SerializeField] private int steps = 20;
     [Range(1f, 100f)]
     [FormerlySerializedAs("_accuracy")][SerializeField] private float accuracy = 100f;
 
-    [HideInInspector] public float maxRange;
+    [HideInInspector] public Vector2 targetPosition;
 
     //Private varibles
     Rigidbody2D playerRb;
     List<Vector2> predictedPlayerPos = new List<Vector2>();
     List<Vector2> predictedBulletPos = new List<Vector2>();
-    Vector2 targetPosition;
-    float bulletSpeed;
-    bool shoot = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        shoot = this.GetComponent<Shoot>();
         player = GameObject.Find("Player").GetComponent<Transform>();
         playerRb = player.GetComponent<Rigidbody2D>();
     }
@@ -35,36 +33,30 @@ public class Aim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawLine(targetPosition, transform.position, Color.black);
-        Debug.DrawLine(player.position, playerRb.velocity.normalized * float.MaxValue, Color.yellow);
+        //Debug.DrawLine(targetPosition, transform.position, Color.black);
+        //Debug.DrawLine(player.position, playerRb.velocity.normalized * float.MaxValue, Color.yellow);
+        PredictTargetPosition();
     }
 
     private void PredictTargetPosition()
     {
         Vector2 targetPos = playerRb.position;
         Vector2 bulletPos = transform.position;
-        bool finished = false;
         predictedPlayerPos.Clear();
         predictedBulletPos.Clear();
-        for(int i = 1; i < steps; i++)
+        for(int i = 1; i < 20; i++)
         {
             //predict target's position
             targetPos += playerRb.velocity * Time.fixedDeltaTime * frames;
             predictedPlayerPos.Add(targetPos);
             //predict bullet's position
             Vector2 shootDir = targetPos - new Vector2(transform.position.x, transform.position.y);
-            bulletPos += shootDir.normalized * bulletSpeed * Time.fixedDeltaTime * frames;
-
+            bulletPos += shootDir.normalized * shoot.bulletSpeed * Time.fixedDeltaTime * frames;
             predictedBulletPos.Add(bulletPos);
         }
+        bool finished = false;
         foreach (var target in predictedPlayerPos)
         {
-            if ((target - new Vector2(transform.position.x, transform.position.y)).magnitude >= maxRange)
-            {
-                targetPos = player.position;
-                finished = true;
-                break;
-            }
             if (finished)
                 break;
             foreach (var position in predictedBulletPos)
@@ -74,11 +66,12 @@ public class Aim : MonoBehaviour
                     //Shoot
                     if (accuracy != 100) targetPosition = CheckAccuracy(target);
                     else targetPosition = target;
-                    shoot = true;
+
                     finished = true;
                     break;
                 }
             }
+            
         }
     }
 
@@ -91,27 +84,16 @@ public class Aim : MonoBehaviour
         return targetPos;
     }
 
-    public Vector2 GetTargetPos(float _speed)
-    {
-        bulletSpeed = _speed;
-        PredictTargetPosition();
-        return targetPosition;
-    }
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.gray;
         Gizmos.DrawSphere(targetPosition, 1f);
-        
+        /*
         foreach (var position in predictedBulletPos)
         {
             Gizmos.color = Color.black;
             Gizmos.DrawSphere(position, 1f);
         }
-        foreach (var position in predictedPlayerPos)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere(position, 1f);
-        }
+        */
     }
 }
--- dev rats game/Assets/Scripts/Enemy/Ai/Shoot.cs
+++ dev rats game/Assets/Scripts/Enemy/Ai/Shoot.cs
@@ -1,51 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using BulletFury;
-using BulletFury.Data;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    [Range(0f, 2f)]
-    [SerializeField] private float smooth;
-    [HideInInspector]public bool canAttack;
-
-    BulletManager _bm;
-    Aim _aim;
-    Vector3 targetPos;
-    Vector2 smoothVelocity;
-    Vector3 playerPos;
-    float bulletSpeed;
-    float deltaSpeed;
-
-    private void Start()
-    {
-        _bm = GetComponent<BulletManager>();
-        bulletSpeed = _bm.GetBulletSettings().Speed;
-        _aim = GetComponent<Aim>();
-        canAttack = false;
-        playerPos = _aim.player.position;
-    }
-
-    private void Update()
-    {
-        if (canAttack)
-        {
-            deltaSpeed = bulletSpeed * Time.deltaTime;
-            targetPos = _aim.GetTargetPos(deltaSpeed);
-            RotateTowardsTarget(targetPos);
-            _bm.GetBulletSettings().SetSpeed(deltaSpeed);
-            _bm.Spawn(transform.position, _bm.Plane == BulletPlane.XY ?
-                transform.up : transform.forward);
-        }
-
-    }
-
-    public void RotateTowardsTarget(Vector3 target)
-    {
-        
-        Debug.DrawLine(transform.position, target);
-        transform.up = Vector2.SmoothDamp(transform.up, target - transform.position,
-            ref smoothVelocity, smooth);
-    }
+    [HideInInspector] public float bulletSpeed;
 }
