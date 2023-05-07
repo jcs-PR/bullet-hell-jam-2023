836df6cd45ac7148d325bb0c987e39c7bcbb8c8d
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-05-07 09:25:00 +0100

    Revert "Fix failed, to revert."
    This reverts commit 8bbbc739b8fb8419f0e44293ed7d47d1f10bfe26.

--- dev rats game/Assets/Scripts/Enemy/Ai/Aim.cs
+++ dev rats game/Assets/Scripts/Enemy/Ai/Aim.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,26 +5,29 @@ using UnityEngine.Serialization;
 
 public class Aim : MonoBehaviour
 {
-    [SerializeField] private Shoot shoot;
-    [SerializeField] private Transform player;
+    [SerializeField] public Transform player;
     [SerializeField] private float playerRadius;
     [Tooltip("frames skiped while predicting position")]
-    [Range(1f, 20f)]
+    [Range(1, 20)]
     [FormerlySerializedAs("_frames")] [SerializeField] private int frames = 10;
+    [Range(1, 100)]
+    [FormerlySerializedAs("_steps")][SerializeField] private int steps = 20;
     [Range(1f, 100f)]
     [FormerlySerializedAs("_accuracy")][SerializeField] private float accuracy = 100f;
 
-    [HideInInspector] public Vector2 targetPosition;
+    [HideInInspector] public float maxRange;
 
     //Private varibles
     Rigidbody2D playerRb;
     List<Vector2> predictedPlayerPos = new List<Vector2>();
     List<Vector2> predictedBulletPos = new List<Vector2>();
+    Vector2 targetPosition;
+    float bulletSpeed;
+    bool shoot = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        shoot = this.GetComponent<Shoot>();
         player = GameObject.Find("Player").GetComponent<Transform>();
         playerRb = player.GetComponent<Rigidbody2D>();
     }
@@ -33,30 +35,36 @@ public class Aim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.DrawLine(targetPosition, transform.position, Color.black);
-        //Debug.DrawLine(player.position, playerRb.velocity.normalized * float.MaxValue, Color.yellow);
-        PredictTargetPosition();
+        Debug.DrawLine(targetPosition, transform.position, Color.black);
+        Debug.DrawLine(player.position, playerRb.velocity.normalized * float.MaxValue, Color.yellow);
     }
 
     private void PredictTargetPosition()
     {
         Vector2 targetPos = playerRb.position;
         Vector2 bulletPos = transform.position;
+        bool finished = false;
         predictedPlayerPos.Clear();
         predictedBulletPos.Clear();
-        for(int i = 1; i < 20; i++)
+        for(int i = 1; i < steps; i++)
         {
             //predict target's position
             targetPos += playerRb.velocity * Time.fixedDeltaTime * frames;
             predictedPlayerPos.Add(targetPos);
             //predict bullet's position
             Vector2 shootDir = targetPos - new Vector2(transform.position.x, transform.position.y);
-            bulletPos += shootDir.normalized * shoot.bulletSpeed * Time.fixedDeltaTime * frames;
+            bulletPos += shootDir.normalized * bulletSpeed * Time.fixedDeltaTime * frames;
+
             predictedBulletPos.Add(bulletPos);
         }
-        bool finished = false;
         foreach (var target in predictedPlayerPos)
         {
+            if ((target - new Vector2(transform.position.x, transform.position.y)).magnitude >= maxRange)
+            {
+                targetPos = player.position;
+                finished = true;
+                break;
+            }
             if (finished)
                 break;
             foreach (var position in predictedBulletPos)
@@ -66,12 +74,11 @@ public class Aim : MonoBehaviour
                     //Shoot
                     if (accuracy != 100) targetPosition = CheckAccuracy(target);
                     else targetPosition = target;
-
+                    shoot = true;
                     finished = true;
                     break;
                 }
             }
-            
         }
     }
 
@@ -84,16 +91,27 @@ public class Aim : MonoBehaviour
         return targetPos;
     }
 
+    public Vector2 GetTargetPos(float _speed)
+    {
+        bulletSpeed = _speed;
+        PredictTargetPosition();
+        return targetPosition;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.gray;
         Gizmos.DrawSphere(targetPosition, 1f);
-        /*
+        
         foreach (var position in predictedBulletPos)
         {
             Gizmos.color = Color.black;
             Gizmos.DrawSphere(position, 1f);
         }
-        */
+        foreach (var position in predictedPlayerPos)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(position, 1f);
+        }
     }
 }
